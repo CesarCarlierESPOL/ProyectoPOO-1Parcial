53029ad8c34e351b6ae41a6356e3a971d30f4d84
Miguel León Monge <33499984+Miguel9872@users.noreply.github.com>
2017-11-16 20:29:17 +0100

    Add files via upload

---
+++ PP1/src/BrujasMagos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel
+ */
+public abstract class BrujasMagos {
+    private String nombre, apellido, varita;
+    private int edad;
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+        return apellido;
+    }
+    public String getVarita(){
+        return varita;
+    }
+    public int getEdad(){
+        return edad;
+    }
+}
